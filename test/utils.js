4e9356418d743c1355aa11e23deb4d5137c01e5f
sharlearn <liewsharlen@gmail.com>
2023-03-06 22:25:23 +0800

    Test.

--- test/utils.js
+++ test/utils.js
@@ -1,5 +1,5 @@
 const { expect } = require("chai");
-const { add } = require("../utils.js");
+const { add, multiply } = require("../utils.js");
 
 describe("Utils", () => {
   describe("Add", () => {
@@ -21,6 +21,28 @@ describe("Utils", () => {
     it("Adds 2 negative numbers", () => {
       const result = add(-1, -1);
       expect(result).to.equal(-2);
+    });
+  });
+
+  describe("Multiply", () => {
+    it("Multiplies 2 of the same number", () => {
+      const result = multiply(2, 2);
+      expect(result).to.equal(4);
+    });
+
+    it("Multiplies 2 different numbers", () => {
+      const result = multiply(2, 4);
+      expect(result).to.equal(8);
+    });
+
+    it("Does multiply 2 negative numbers", () => {
+      const result = multiply(-2, -2);
+      expect(result).to.equal(4);
+    });
+
+    it("Also multiples a negative and positive number", () => {
+      const result = multiply(-2, 6);
+      expect(result).to.equal(-12);
     });
   });
 });
--- utils.js
+++ utils.js
@@ -2,6 +2,11 @@ const add = (a, b) => {
   return a + b;
 };
 
+const multiply = (a, b) => {
+  return a * b;
+};
+
 module.exports = {
   add,
+  multiply,
 };
